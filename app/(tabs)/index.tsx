06b28d031afa684711b45b3e30c5c6751814f6ea
Rui Marcos João <ruimarcosjoao@gmail.com>
2024-02-21 02:46:58 +0100

    feat: create a simple app of note

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,51 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Link } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as Linking from "expo-linking";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View className="flex-1 items-center justify-center p-2 bg-neutral-950  ">
+      <Image
+        source={{
+          uri: "https://avatars.githubusercontent.com/u/69318293?v=4",
+        }}
+        className="w-48 h-48 rounded-full"
+      />
+      <Text className="text-2xl font-bold mt-4">Rui marcos</Text>
+      <Text className="text-base mt-2 text-gray-500">Luanda, Angola</Text>
+      <Text className="text-base mt-4 text-gray-500">
+        Desenvolvedor(a) mobile apaixonado(a) por criar aplicativos intuitivos e
+        bonitos. Criei este aplicativo de explo para ajudar a comunidade para
+        escrever notas e exportar em pdf, quem quiser ajudar a sua ajudar é bem
+        vinda.
+      </Text>
+      <Text className="text-base mt-4 text-yellow-500">De momento gerar pdf só funciona para Android</Text>
+      <View className="flex flex-row mt-8">
+        <TouchableOpacity
+          className="bg-blue-500 p-4 h-12 rounded-md mx-2"
+          onPress={() => {
+            Linking.openURL("https://twitter.com/Ruimarcosjoao");
+          }}
+        >
+          <Text className="text-white text-center">Twitter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="bg-blue-500 p-4 h-12 rounded-md mx-2"
+          onPress={() => {
+            Linking.openURL("https://www.linkedin.com/in/rui-marcos-joao/");
+          }}
+        >
+          <Text className="text-white text-center">LinkedIn</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="bg-blue-500 p-4 h-12 rounded-md mx-2"
+          onPress={() => {
+            Linking.openURL("https://github.com/ruimarcosjoao");
+          }}
+        >
+          <Text className="text-white text-center">Github</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,31 +1,85 @@
-import { StyleSheet } from 'react-native';
+import React from "react";
+import {
+  Text,
+  SafeAreaView,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+} from "react-native";
+import * as Print from "expo-print";
+import { shareAsync } from "expo-sharing";
+import {
+  RichText,
+  Toolbar,
+  useEditorBridge,
+  darkEditorTheme,
+  darkEditorCss,
+  TenTapStartKit,
+  CoreBridge,
+} from "@10play/tentap-editor";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+
+const initialContent = '<p>Olá bem vindo ao notebook mobile</p>'
+
+function createhtml(content:string) {
+  return `
+  <html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+    </head>
+    <body style="text-align: center;">
+        ${content}
+    </body>
+  </html>
+  `
+}
 
 export default function TabTwoScreen() {
+
+  const editor = useEditorBridge({
+    autofocus: true,
+    avoidIosKeyboard: true,
+    initialContent: initialContent,
+  });
+
+  const printToFile = async (html: string) => {
+    
+    const { uri } = await Print.printToFileAsync({ html });
+    console.log("File has been saved to:", uri);
+    await shareAsync(uri, { UTI: ".pdf", mimeType: "application/pdf" });
+    return uri
+  };
+
+
+  async function getHtmlToPDF() {
+    const content = await editor.getHTML();
+    const html = createhtml(content)
+
+    printToFile(html);
+    
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <SafeAreaView className="flex-1 px-2">
+      <View className="flex-1">
+        <RichText editor={editor} />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          className="absolute w-full bottom-0"
+        >
+          <Toolbar editor={editor} />
+        </KeyboardAvoidingView>
+      </View>
+      <View>
+        <Pressable
+          onPress={getHtmlToPDF}
+          className="px-4 py-4 focus:scale-50 rounded-md bg-yellow-600 flex items-center justify-center"
+        >
+          <Text className="text-white">Gerar PDF</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
